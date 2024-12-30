15f2e49507fc037f71890b3625e69da40800cd2b
DevGirlAos40 <devgirlaos40@gmail.com>
2024-12-30 15:31:44 -0300

    Inclusão de regra que torna a pesquisa Case Insensitive

--- js/ocultaItens.js
+++ js/ocultaItens.js
@@ -9,7 +9,7 @@ export function ocultaItens(listaComprar, listaComprados, valorDigitado) {
     //para cada item (do tipo li) da lista comprados inclui na variável itens o elemento parágrafo do item (onde o nome do item está)
     listaComprados.querySelectorAll('li').forEach(item => { itens.push(item.querySelector('p')); });    
     //armazena na variável intesOcultar todos os itens que não correspondem a busca realizada
-    itensOcultar = itens.filter(item => !item.textContent.includes(valorDigitado));
+    itensOcultar = itens.filter(item => !item.textContent.toLowerCase().includes(valorDigitado.toLowerCase()));
     //para cada de itensOcultar
     itensOcultar.forEach(item => {
         //definir o display do item como None (para que seja ocultado da lista e somente os itens que correspondem a busca permaneçam na tela)
