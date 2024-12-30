0d84233f1baa962ca2a61bbf2955e74cd5634533
Vivi Santana - Dev Girl Aos 40 <devgirlaos40@gmail.com>
2024-12-30 15:55:06 +0000

    Inclusão da funcionalidade buscaItem

---
+++ js/buscaItem.js
@@ -0,0 +1,28 @@
+import { limpaInput } from "./limpaInput.js";
+import { ocultaItens } from "./ocultaItens.js";
+import { exibeListaCompleta } from "./exibeListaCompleta.js";
+
+const listaComprar = document.getElementById("lista-comprar");//seleciona as uls de comprar
+const listaComprados = document.getElementById("lista-comprados");//seleciona as uls de comprar
+const inputItem = document.getElementById("input-item");//seleciona o campos input onde o usuário informou o filtro da busca
+
+const botaoIncluir = document.getElementById("botao-salvar-item");
+const botaoVoltar = document.getElementById("botao-voltar");
+
+export function buscaItem(evento) {
+    //não atualiza a página após o click do botão
+    evento.preventDefault();
+    /*BUSCA */
+    let valorDigitado = inputItem.value; //recupera o valor digitado pelo usuário
+    
+    if (valorDigitado === "" || valorDigitado.trim() === "") { //se o usuário não digitou nada
+        alert("Favor informar o nome do item que deseja pesquisar:"); //exibe alerta pedindo para digitar
+    }else{ 
+        exibeListaCompleta(listaComprar, listaComprados);//se o usuário digitou algo
+        ocultaItens(listaComprar, listaComprados, valorDigitado); //oculta os itens que não correspondem ao valor digitado
+        limpaInput();//limpa o campo input
+        botaoVoltar.style.display = "block";
+        botaoIncluir.style.display = "none";
+    }
+}
+
